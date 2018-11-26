986f76f82b16e2f860aa149e2c7353bdb3fc787b
JR <radtanaporn.kn@gmail.com>
2018-11-26 17:20:51 +0700

    renponseData

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <h1>Product List</h1>
-       
+        <ol>
+            <li v-for="item in products" :key="item.id">
+                {{item.id}} {{item.title}} {{item.price}} 
+            </li>
+        </ol>
     </div>
 </template>
 <script>
@@ -10,14 +14,18 @@ export default {
   name: 'products',
   data(){
       return {
-          message: 'Project2'
+          message: 'Project2',
+          products: []
       }
   },
   mounted () {
+      var instance = this
+      //this.products อ้างถึง instance
     axios
       .get('https://fathomless-wave-75295.herokuapp.com/api/products')
       .then(function(response){
             console.log(response.data)
+            instance.products = response.data.data
       })
 }}
     
