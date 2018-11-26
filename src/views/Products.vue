e30a4576cbb4a19bc2cbd3520346c1041ef794f8
JR <radtanaporn.kn@gmail.com>
2018-11-26 17:04:37 +0700

    test

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,5 +1,26 @@
 <template>
     <div>
         <h1>Product List</h1>
+       
     </div>
 </template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'products',
+  data(){
+      return {
+          message: 'Project2'
+      }
+  },
+  mounted () {
+    axios
+      .get('https://fathomless-wave-75295.herokuapp.com/api/products')
+      .then(function(response){
+            console.log(response.data)
+      })
+}}
+    
+
+</script>
+
